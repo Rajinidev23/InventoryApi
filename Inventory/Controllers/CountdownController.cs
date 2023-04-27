6df8ef94165a28f26b581dd3772ff7221f30f7b8
Rajini Kunchala <rajinidev23@gmail.com>
2023-04-26 22:37:13 -0400

    Add project files.

---
+++ Inventory/Controllers/CountdownController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Timers;
+
+namespace Inventory.Controllers
+{
+    public class CountdownController : ApiController
+    {
+        public CountdownController()
+        {
+            countdownTimer = new Timer(1000); // timer interval is 1 second
+           
+        }       
+
+        private Timer countdownTimer;
+        private int countdownDuration = 60; // duration in seconds
+
+        [HttpGet]
+        public IHttpActionResult Start()
+        {            
+            countdownTimer.Start();
+            var obj = new InventoryEntities();
+            var items = obj.tblItems.ToList();
+            var response = new MyResponse { MyItemList = items, CountDown = countdownDuration , StartTime=DateTime.Now.ToString()};
+
+            return Ok(response);
+        }
+
+    }
+}
---
+++ Inventory/MyResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventory
+{
+    public class MyResponse
+    {
+        public List<tblItem> MyItemList { get; set; }
+        public string StartTime { get; set; }
+        public int CountDown { get; set; }
+
+    }
+}
